96b458f4cfdf2e6cb56895b7ab1ca7a3573de76d
Asjal Amir <113906443+mohdasjal1@users.noreply.github.com>
2024-07-10 20:40:14 +0500

    Update Q19.asm

--- Q19.asm
+++ Q19.asm
@@ -5,26 +5,40 @@
 .data   
     prompt1 db 'Input an alphabet : $'
     prompt2 db 10, 13, 'Invalid Input! $'
-
-    
+    prompt3 db 'Its equivalent small letter is : $'
+    prompt4 db 'Its equivalent capital letter is : $'
 
 .code
+
+print macro p1
     
+    mov dx, offset p1
+    mov ah, 9
+    int 21h    
+        
+endm        
+
+newline proc
     
+    mov dx, 10
+    mov ah, 2
+    int 21h
+    
+    mov dx, 13
+    mov ah, 2
+    int 21h
+        
+    ret
+newline endp
+        
 main proc
     mov ax, @data
     mov ds, ax    
-    
-    mov dx, offset prompt1
-    mov ah, 9
-    int 21h 
-    
+     
+    print prompt1 
     
     mov ah, 1
-    int 21h
-    
-    
-    
+    int 21h   
     mov dl, al
     
 check_capital:
@@ -41,7 +55,13 @@ check_small:
     jbe is_small
     
 is_capital:   
-    add dl, 32    
+    add dl, 32
+    push dx
+    
+    call newline
+    print prompt3
+    
+    pop dx    
     mov ah, 2
     int 21h
     
@@ -49,15 +69,20 @@ is_capital:
 is_small:       
        
     sub dl, 32      
+    push dx
+    
+    call newline 
+    print prompt4
+    
+    pop dx    
+    
     mov ah, 2
     int 21h
     
     jmp exit      
     
 error:
-    mov dx, offset prompt2
-    mov ah, 9
-    int 21h 
+    print prompt2
     
         
     
@@ -67,7 +92,7 @@ exit:
     int 21h    
     
     
-    ;65 - 90  A - Z   32
+    ;65 - 90  A - Z  , 97 - 65 = 32
     ;97 - 122 a - z
     
      
